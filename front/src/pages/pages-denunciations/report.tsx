693bd586ad250f7cec65e988f6e85a961e16fdc1
Caio Nunes <caionuneso@hotmail.com>
2021-01-08 20:54:35 -0300

    lasttest

--- front/src/pages/pages-denunciations/report.tsx
+++ front/src/pages/pages-denunciations/report.tsx
@@ -7,36 +7,34 @@ import '../../styles/pages/pages-denunciations/report.css';
 function report(){
     return(
             <div id="page-report">
-           
-            <aside>
-                <header>
-                    <div className="img-logo">
-                         <img src="/static/media/icon.5d399b31.svg" alt="icon" />
+                <aside>
+                    <header>
+                        <div className="img-logo">
+                            <img src="/static/media/icon.5d399b31.svg" alt="icon" />
+                        </div>
+            
+                        <h2>Com a sua contribuição conseguimos transformar nossa cidade em um lugar melhor</h2>
+                        <p>Você também pode adicionar imagens e vídeos à denúncia</p>
+
+                    </header>
+
+                    <footer>
+                        <strong>Estado (UF)</strong>
+                        <span>Cidade</span>
+                    </footer>
+                </aside>
+                <div className="rightBlockReport">
+
+                    <div className="styleTitleBlock">
+                        <h2>Descreva, com detalhamento, a sua denúncia</h2>
                     </div>
-        
-                    <h2>Com a sua contribuição conseguimos transformar nossa cidade em um lugar melhor</h2>
-                    <p>Você também pode adicionar imagens e vídeos à denúncia</p>
-
-                </header>
-
-                <footer>
-                    <strong>Estado (UF)</strong>
-                    <span>Cidade</span>
-                </footer>
-            </aside>
-            <div className="rightBlockReport">
-
-                <div className="styleTitleBlock">
-                    <h2>Descreva, com detalhamento, a sua denúncia</h2>
-                </div>
-
-                <div className="blockReportForm">
+            
+                    <div className="blockReportForm">
                     
+                    </div>
                     
-
                 </div>
             </div>
-        </div>
 
     );
 }
